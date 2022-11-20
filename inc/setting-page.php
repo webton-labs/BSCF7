5ed8474c2760661febd2b27b66eb9cbcf5d7b145
mrngstr-labs <vladzorin.audiorecords@gmail.com>
2022-11-21 00:37:12 +0300

    0.16.3 ⬆️
    Добавлены функции:
    - Удаление AutoP;
    - Удаление враппера вокруг элементов;
    - Подключение стилей и скриптов, только на страницах с [шорткодом] формы.

---
+++ inc/setting-page.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Страница настроек плагина
+ * 
+ * @package bootstrap-cf7-main
+ * @version 0.16.3
+ */
+
+// регистрация пункта меню
+add_action( 'admin_menu', 'bscf7_settings_menu_page', 25 );
+function bscf7_settings_menu_page() { 
+	add_submenu_page(
+		'wpcf7',
+		__('Настройки BSCF7', 'bootstrap_cf7'),
+		__('Настройки BSCF7', 'bootstrap_cf7'),
+		'manage_options',
+		'bscf7_settings',
+		'bscf7_setting_page_callback'
+	);
+}
+ 
+// Каллбек функция
+function bscf7_setting_page_callback(){
+	echo '<div class="wrap">
+	<h1>' . get_admin_page_title() . '</h1>
+	<p>' . __('Страница настроек плагина BSCF7', 'bootstrap_cf7') . '</p>
+	<form method="post" action="options.php">';
+ 
+		settings_fields( 'bscf7_theme_settings' );
+		do_settings_sections( 'bscf7_settings' );
+		submit_button();
+ 
+	echo '</form></div>';
+}
+add_action( 'admin_init',  'bscf7_settings_fields' );
+
+// Поля настроек
+function bscf7_settings_fields(){
+    // Auto <P>
+	add_settings_section(
+		'bscf7_settings_section_rest', 
+		__(''),
+		'',
+		'bscf7_settings'
+	);
+
+	// Начало
+    register_setting('bscf7_theme_settings', 'autop_disable');
+    add_settings_field(
+		'autop_disable',
+		__('Отключение AutoP', 'bootstrap_cf7'),
+		'autop_disable_field',
+		'bscf7_settings',
+		'bscf7_settings_section_rest',
+		array( 
+			'label_for' => 'autop_disable',
+			'class' => '',
+			'name' => 'autop_disable',
+		)
+	);
+	function autop_disable_field( $args ){
+		// получаем значение из базы данных
+		$value = get_option( $args[ 'name' ] );
+		
+		echo '<div class="form-check form-switch"><input id="autop_disable" name="autop_disable" type="checkbox" 
+		' . checked( 1, get_option( 'autop_disable' ), false ) . '
+		value="1" class="form-check-input"
+		><label class="form-check-label" for="autop_disable">' . __('Отключение автоматической обертки всех полей в тег "p".', 'bootstrap_cf7') .'</label></div>';
+	 
+	}
+    // Конец
+
+	// Начало
+	register_setting('bscf7_theme_settings', 'wrapper_disable');
+	add_settings_field(
+		'wrapper_disable',
+		__('Отключение враппера элементов', 'bootstrap_cf7'),
+		'wrapper_disable_field',
+		'bscf7_settings',
+		'bscf7_settings_section_rest',
+		array( 
+			'label_for' => 'wrapper_disable',
+			'class' => '',
+			'name' => 'wrapper_disable',
+		)
+	);
+	function wrapper_disable_field( $args ){
+		// получаем значение из базы данных
+		$value = get_option( $args[ 'name' ] );
+		
+		echo '<div class="form-check form-switch"><input id="wrapper_disable" name="wrapper_disable" type="checkbox" 
+		' . checked( 1, get_option( 'wrapper_disable' ), false ) . '
+		value="1" class="form-check-input"
+		><label class="form-check-label" for="wrapper_disable">' . __('Отключение враппера вокруг элементов CF7', 'bootstrap_cf7') .'
+		<br><b>' . __('Внимание! ', 'bootstrap_cf7') .'</b>' . __('Удаление враппера, нарушает работу функции валидации', 'bootstrap_cf7') . '</label></div>';
+		
+	}
+	// Конец
+}
+
+?>
--- main.php
+++ main.php
@@ -5,20 +5,62 @@
  * Description: Добавляет в CF7 поддержку компонентов Bootstrap (алерты, радио, чекбоксы) и дополнительные функции. Требует установленного плагина Contact Form 7.
  * Author: Вебтон.ру
  * Author URI: https://webton.ru
- * Version: 0.16.1
+ * Version: 0.16.3
  */
 
-// ================== Проверка обновлений плагина ================== //
-    require_once __DIR__ . '/update/update-checker.php';
-    use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+// Проверка обновлений плагина 
+ 
+require_once __DIR__ . '/update/update-checker.php';
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
-    $BCF7_UpdateChecker = PucFactory::buildUpdateChecker(
-        'https://github.com/Webton-Network/bootstrap-cf7-main',
-        __FILE__,
-        'bootstrap-cf7-main'
-    );
+$BCF7_UpdateChecker = PucFactory::buildUpdateChecker(
+    'https://github.com/Webton-Network/bootstrap-cf7-main',
+    __FILE__,
+    'bootstrap-cf7-main'
+);
 
-    $BCF7_UpdateChecker->setBranch('main');
-    $BCF7_UpdateChecker->setAuthentication('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');
-// ============================= КОНЕЦ ============================= //
+$BCF7_UpdateChecker->setBranch('main');
+$BCF7_UpdateChecker->setAuthentication('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');
+
+// КОНЕЦ
+
+// Подключение страницы настроек
+require_once __DIR__ . '/inc/setting-page.php';
+
+// Отключение AutoP
+if (get_option( 'autop_disable' ) == 1) {
+    add_action( 'wpcf7_autop_or_not', '__return_false' );
+}
+// Конец
+
+// Подключение стилей и сриптов CF7 только на страницах, где выведен [шорткод]
+function wpcf7_remove_assets() {
+    add_filter( 'wpcf7_load_js', '__return_false' );
+    add_filter( 'wpcf7_load_css', '__return_false' );
+    add_filter( 'wpcf7cf_load_js', '__return_false' );
+    add_filter( 'wpcf7cf_load_css', '__return_false' );
+}
+add_action( 'wpcf7_init', 'wpcf7_remove_assets' );
+
+function wpcf7_add_assets( $atts ) {
+    wpcf7_enqueue_styles();
+    wpcf7_enqueue_scripts();
+    if ( !is_front_page() && !is_home() ) {
+        wpcf7cf_enqueue_scripts();
+        wpcf7cf_enqueue_styles();
+    }
+    return $atts;
+}
+add_filter( 'shortcode_atts_wpcf7', 'wpcf7_add_assets' );
+// Конец
+
+// Удаление враппера вокруг элементов форм
+if (get_option( 'wrapper_disable' ) == 1) {
+    add_filter('wpcf7_form_elements', function($content) {
+        $content = preg_replace('/<(span).*?class="\s*(?:.*\s)?wpcf7-form-control-wrap(?:\s[^"]+)?\s*"[^\>]*>(.*)<\/\1>/i', '\2', $content);
+        return $content;
+    });
+}
+// Конец
+
 ?>
